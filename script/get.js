1e809c093f6c8edfc1b87b52fbbd93095cd6bb55
Samuel Soderberg <samuel@samuelsoderberg.se>
2023-11-22 06:52:55 +0100

    ...

--- script/get.js
+++ script/get.js
@@ -10,8 +10,7 @@ export function get() {
   let search = document.getElementById("search")?.value;
 
   return fetch(
-    "https://nutrition-by-api-ninjas.p.rapidapi.com/v1/nutrition?query=soy%20bean%2C%20peanut%2C%20sesame%20seed%2C%20lentil%2C%20chick%20pea%2C%20%20bean%2C%20%2C%20egg%2C%20Milk%2C%20sugar%2C%20" +
-      search,
+    "https://nutrition-by-api-ninjas.p.rapidapi.com/v1/nutrition?query=soy%20bean%2C%20peanut%2C%20sesame%20seed%2C%20lentil%2C%20chick%20pea%2C%20%20bean%2C%20%2C%20egg%2C%20Milk%2C%20sugar%2C%20" + search,
     options
   )
     .then(handleError)
